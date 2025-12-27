6fc326d17ba4c55ce74c01644b786e4968e4047d
Alexander Markovski <alexandermarkovski@Alexanders-MacBook-Air.local>
2025-12-26 20:33:15 -0800

    Fix Django configuration and add initial migrations

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/alexandermarkovski/Documents/.venv"
+set -gx VIRTUAL_ENV "/Users/alexandermarkovski/Coding/OG Agora/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
