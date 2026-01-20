1e442615de784daf3c6ba9cc9764afbc94a5ce91
Alexander Markovski <alexandermarkovski@Alexanders-MacBook-Air.local>
2026-01-19 22:48:41 -0700

    committed correctly

--- Customer/api/forms.py
+++ Customer/api/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import Referral
+from .models import Referral, Offer
 
 class ReferralForm(forms.ModelForm):
     class Meta:
         model = Referral
-        fields = ['cpa', 'offer', 'client_email']
+        # REMOVE 'cpa' from this list!
+        fields = ['offer', 'client_email']
+
+    # This ensures users can't pick "Dead" offers
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['offer'].queryset = Offer.objects.filter(is_active=True)
